6123770d1e23688bf056dd13bc676c1767fa7f16
Man Mai <42661871+maimand@users.noreply.github.com>
2022-12-12 14:37:31 +0700

    Fix bugs

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,26 +9,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //key: context.read<MenuController>().scaffoldKey,
-      drawer: SideMenu(),
-      body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // We want this side menu only for large screen
-            if (Responsive.isDesktop(context))
+    return SelectionArea(
+      child: Scaffold(
+        //key: context.read<MenuController>().scaffoldKey,
+        drawer: SideMenu(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // We want this side menu only for large screen
+              if (Responsive.isDesktop(context))
+                Expanded(
+                  // default flex = 1
+                  // and it takes 1/6 part of the screen
+                  child: SideMenu(),
+                ),
               Expanded(
-                // default flex = 1
-                // and it takes 1/6 part of the screen
-                child: SideMenu(),
+                // It takes 5/6 part of the screen
+                flex: 5,
+                child: DashboardScreen(),
               ),
-            Expanded(
-              // It takes 5/6 part of the screen
-              flex: 5,
-              child: DashboardScreen(),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
